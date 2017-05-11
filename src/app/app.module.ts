57a8d31d737b2c4837772e2ffabccee54e54491c
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-05-11 20:54:15 +0300

    Refactor import/export braces code style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import {NgModule}       from '@angular/core';
-import {BrowserModule}  from '@angular/platform-browser';
-import {FormsModule}    from '@angular/forms';
-import {RouterModule}   from '@angular/router';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
-import {AppComponent}        from './app.component';
-import {HeroDetailComponent} from './hero-detail.component';
-import {HeroesComponent}     from './heroes.component';
-import {HeroService}         from './hero.service';
-import {DashboardComponent} from "./dashboard.component";
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { DashboardComponent } from "./dashboard.component";
 
 @NgModule({
   imports: [
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {Hero} from './hero';
-import {HeroService} from './hero.service'
+import { Hero } from './hero';
+import { HeroService } from './hero.service'
 
 @Component({
   selector: 'my-heroes',
-  template: `    
+  template: `
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
