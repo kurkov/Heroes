9098457ea5b8138a6a754048cb3b78aed99f3600
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-05-13 23:29:18 +0300

    Add the ability to save hero details

--- src/app/hero.service.js
+++ src/app/hero.service.js
@@ -14,6 +14,7 @@ require("rxjs/add/operator/toPromise");
 var HeroService = (function () {
     function HeroService(http) {
         this.http = http;
+        this.headers = new http_1.Headers({ 'Content-Type': 'application/json' });
         this.heroesUrl = 'api/heroes'; // URL to web api
     }
     HeroService.prototype.getHeroes = function () {
@@ -34,6 +35,14 @@ var HeroService = (function () {
         return this.http.get(url)
             .toPromise()
             .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.update = function (hero) {
+        var url = this.heroesUrl + "/" + hero.id;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(function () { return hero; })
             .catch(this.handleError);
     };
     HeroService.prototype.handleError = function (error) {
