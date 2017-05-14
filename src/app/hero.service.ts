2cc77d47031123ab0b705c333ab701ab2fad023d
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-05-14 17:15:35 +0300

    Add the ability to add heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,7 +8,7 @@ import { Hero } from "./hero";
 @Injectable()
 export class HeroService {
 
-  private headers = new Headers({'Content-Type': 'application/json'});
+  private headers = new Headers({ 'Content-Type': 'application/json' });
   private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(private http: Http) {
@@ -36,10 +36,18 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
   update(hero: Hero): Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
-      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .put(url, JSON.stringify(hero), { headers: this.headers })
       .toPromise()
       .then(() => hero)
       .catch(this.handleError);
