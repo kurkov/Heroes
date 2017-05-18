7bbebacbaf2ac78943e81a3bc48380a0965be78b
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-05-18 15:05:05 +0300

    Add the search component to the dashboard

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -18,6 +18,7 @@ var dashboard_component_1 = require("./dashboard.component");
 var hero_detail_component_1 = require("./hero-detail.component");
 var heroes_component_1 = require("./heroes.component");
 var hero_service_1 = require("./hero.service");
+var hero_search_component_1 = require("./hero-search.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -37,6 +38,7 @@ AppModule = __decorate([
             dashboard_component_1.DashboardComponent,
             hero_detail_component_1.HeroDetailComponent,
             heroes_component_1.HeroesComponent,
+            hero_search_component_1.HeroSearchComponent
         ],
         providers: [hero_service_1.HeroService],
         bootstrap: [app_component_1.AppComponent]
